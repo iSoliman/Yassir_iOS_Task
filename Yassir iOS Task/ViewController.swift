f7282ceb54e9916c1d7d837c02c65cf84834c08d
islam Soliman <islam.seliman92@gmail.com>
2024-09-21 11:13:19 +0300

    Initial Commit

---
+++ Yassir iOS Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yassir iOS Task
+//
+//  Created by Islam Soliman on 21/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
